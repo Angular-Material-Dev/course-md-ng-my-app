dd25f9dcc3f9a640c64ad4e488f1ec115af21c5d
Dharmen Shah <shhdharmen@gmail.com>
2024-05-24 20:11:23 +0530

    fix: remove stackblitz fix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'zone.js/dist/zone';
+// import 'zone.js/dist/zone';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
